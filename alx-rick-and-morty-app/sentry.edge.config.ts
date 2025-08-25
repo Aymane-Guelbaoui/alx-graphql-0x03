5628116050a0f2c0b766e3ee334b3a1d18e24b5b
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-08-25 12:26:06 +0100

    Create sentry.edge.config.ts

---
+++ alx-rick-and-morty-app/sentry.edge.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5f3297244e5b62e83a69eb3c0c0fb552@o4509895699988480.ingest.de.sentry.io/4509895701364816",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
