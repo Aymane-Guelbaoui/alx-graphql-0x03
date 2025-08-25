6df055d695322f8325dce33ce63626f6bfe9a60e
Aymane-Guelbaoui <aymaneguelbaoui@gmail.com>
2025-08-25 12:27:20 +0100

    Create sentry.server.config.ts

---
+++ alx-rick-and-morty-app/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5f3297244e5b62e83a69eb3c0c0fb552@o4509895699988480.ingest.de.sentry.io/4509895701364816",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
